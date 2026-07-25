912ea7769031b1949f6a03780ac48e79d2d117c6
Ramfour <bdddanil2002@gmail.com>
2026-07-25 12:35:58 +0700

    add functions.php: new functions for formatting string

---
+++ src/helpers/functions.php
@@ -0,0 +1,45 @@
+<?php
+/**
+ * Принимает как аргумент три строки — фамилию, имя и отчество. 
+ * Возвращает как результат их же, но склеенные через пробел.
+ *
+ * @param string $surname Фамилия
+ * @param string $name Имя
+ * @param string $patronymic Отчество
+ * @return string Склеенное полное имя
+ */
+function getFullnameFromParts (string $surname, string $name, string $patronymic): string
+{
+    return "$surname $name $patronymic";
+
+}
+
+/**
+ * Принимает как аргумент полное имя, состоящее из трёх слов. 
+ * Возвращает как результат массив, в котором под ключами 'surname', 'name' и 'patronymic' 
+ * будут лежать соответствующие части полного имени.
+ *
+ * @param string $fullname Полное имя
+ * @return array Массив с частями полного имени
+ */
+function getPartsFromFullname (string $fullname): array
+{
+    $keys = ['surname', 'name', 'patronymic'];
+    $values = explode(' ', $fullname);
+    return array_combine($keys, $values);
+}
+
+/**
+ * Принимает как аргумент полное имя, состоящее из трёх слов. 
+ * Возвращает как результат строку, состоящую из имени и инициалов.
+ *
+ * @param string $fullname Полное имя
+ * @return string Имя и инициалы фамилии
+ */
+function getShortName (string $fullname): string
+{
+    $parts = getPartsFromFullname($fullname);
+    return $parts['name'] . ' ' . mb_substr($parts['surname'], 0, 1) . '.';
+}
+
+
