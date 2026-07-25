311b0b154e32e7cfd56df2bf8c8596be61da1924
Ramfour <bdddanil2002@gmail.com>
2026-07-25 12:01:46 +0700

    add folders arhitecture

---
+++ public/index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    require __DIR__ . '/../data/arrays.php';
+    print_r($example_persons_array);
+</body>
+</html>
