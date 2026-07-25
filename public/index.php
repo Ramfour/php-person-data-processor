4c6b81b2c86c526ba25c3d303b48fcc68c6a800e
Ramfour <bdddanil2002@gmail.com>
2026-07-25 22:23:54 +0700

    add readme; licence; ; new function functions.php  getPerfectPartner | fix functions.php: getGenderFromName

--- public/index.php
+++ public/index.php
@@ -12,6 +12,11 @@
     require __DIR__ . '/../data/arrays.php';
     require __DIR__ . '/../src/helpers/functions.php';
     echo getGenderDescription($examplePersonsArray, $rules);
+    /** 
+     * @var array $persons {@see arrays.php}
+     * @var array $rules {@see arrays.php}
+     */
+    echo getPerfectPartner('Иванов', 'Иван', 'Иванович', $examplePersonsArray, $rules);
     ?>
 </body>
 
--- src/helpers/functions.php
+++ src/helpers/functions.php
@@ -66,7 +66,9 @@ function getGenderFromName(string $fullname, array $rules): int
             }
         }
     }
-    return $sexChar;
+    if ($sexChar > 0) return 1;
+    if ($sexChar < 0) return -1;
+    return 0;
 }
 /**
  * Принимает как аргумент массив людей, состоящий из полных имён и должностей. 
@@ -98,4 +100,69 @@ function getGenderDescription(array $persons, array $rules): string
                 </pre>
                 RESULT;
     return $result;
+}
+
+/**
+ * Принимает как аргумент строку. 
+ * Возвращает как результат строку с первой буквой в верхнем регистре, 
+ * а остальными буквами в нижнем регистре.
+ *
+ * @param string $string Строка
+ * @return string Строка с первой буквой в верхнем регистре
+ */
+function getFirstCharUpper(string $string): string
+{
+    if ($string === '') {
+        return '';
+    }
+    $firstChar = mb_substr($string, 0, 1, 'UTF-8');
+    $restOfString = mb_strtolower(mb_substr($string, 1, null, 'UTF-8'), 'UTF-8');
+    return mb_strtoupper($firstChar, 'UTF-8') . $restOfString;
+}
+
+/**
+ * Принимает как аргумент массив людей, состоящий из полных имён и должностей. 
+ * Возвращает как результат случайного человека из массива.
+ *
+ * @param array $persons Массив людей
+ * @return array Случайный человек из массива
+ */
+function takeRandomPerson(array $persons): array
+{
+    return $persons[array_rand($persons)];
+}
+
+/**
+ * Принимает как аргумент фамилию, имя и отчество, массив людей и массив правил для определения пола по имени. 
+ * Возвращает как результат строку с идеальной парой.
+ *
+ * @param string $surname Фамилия
+ * @param string $name Имя
+ * @param string $patronymic Отчество
+ * @param array $persons Массив людей
+ * @param array $rules Массив правил для определения пола по имени
+ * @return string Строка с идеальной парой
+ */
+function getPerfectPartner(string $surname, string $name, string $patronymic, array $persons, array $rules): string
+{
+    $whileCounter = 0;
+    $fullname = getFullnameFromParts(
+        getFirstCharUpper($surname), 
+        getFirstCharUpper($name), 
+        getFirstCharUpper($patronymic));
+    $gender = getGenderFromName($fullname, $rules);
+    while (true) {
+        $whileCounter++;
+        if ($whileCounter > 1000) {
+            return 'Не удалось найти идеальную пару после 1000 попыток.';
+        }
+        $randomPerson = takeRandomPerson($persons);
+        $randomGender = getGenderFromName($randomPerson['fullname'], $rules);
+        if ($gender * $randomGender < 0) {
+            $firstPersonShortName = getShortName($fullname);
+            $secondPersonShortName = getShortName($randomPerson['fullname']);
+            $randomPercent = rand(5000, 10000) / 100;
+            return $firstPersonShortName . ' + ' . $secondPersonShortName . ' =' . PHP_EOL . '♡ Идеально на ' . $randomPercent . '% ♡';
+        }
+    }
 }
